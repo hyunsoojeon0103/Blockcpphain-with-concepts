347fb20fae2432b6a98885676d2456c0ec91df1b
hyun <hyunsoojeon0103@gmail.com>
2020-05-03 14:21:17 -0400

    put Block and Blockchains into template

--- Blockchain.cpp
+++ Blockchain.cpp
@@ -1,81 +1,97 @@
-#include "Blockchain.h"
-Blockchain::Blockchain(const uint32_t difficulty)
+template <class T>
+Blockchain<T>::Blockchain(const uint64_t difficulty)
     : n_difficulty(difficulty)
 {
-    add_block(Block());
+    add_block(Block<T>());
 }
 
-Blockchain::~Blockchain()
+template <class T>
+Blockchain<T>::~Blockchain()
 {
 }
-void Blockchain::add_block(Block b_new)
+
+template <class T>
+void Blockchain<T>::add_block(Block<T> b_new)
 {
     b_new.mine_block(n_difficulty);
-    b_new.set_prev_hash(v_chain.size() > 0 ? get_last_block().get_hash() : "0");
+    b_new.set_prev_hash(v_chain.size() > 0 ? get_last_block().get_hash() : T());
     b_new.set_n_index(v_chain.size());
     v_chain.push_back(b_new);
 }
-void Blockchain::set_n_difficulty(const uint32_t difficulty)
+template <class T>
+void Blockchain<T>::set_n_difficulty(const uint64_t difficulty)
 {
     assert(difficulty >= 0 && difficulty < 64);
     n_difficulty = difficulty;
 }
-uint32_t Blockchain::get_n_difficulty() const
+template <class T>
+uint64_t Blockchain<T>::get_n_difficulty() const
 {
     return n_difficulty;
 }
-size_t Blockchain::size() const
+template <class T>
+size_t Blockchain<T>::size() const
 {
     return v_chain.size();
 }
-Block &Blockchain::operator[](const uint32_t idx)
+template <class T>
+Block<T>& Blockchain<T>::operator[](const uint64_t idx)
 {
     assert(idx < v_chain.size());
     return v_chain[idx];
 }
-const Block &Blockchain::operator[](const uint32_t idx) const
+template <class T>
+const Block<T>& Blockchain<T>::operator[](const uint64_t idx) const
 {
     assert(idx < v_chain.size());
     return v_chain[idx];
 }
-std::vector<Block>::iterator Blockchain::begin()
+template <class T>
+std::vector<Block<T>>::iterator Blockchain<T>::begin()
 {
     return v_chain.begin();
 }
-std::vector<Block>::iterator Blockchain::end()
+template <class T>
+std::vector<Block<T>>::iterator Blockchain<T>::end()
 {
     return v_chain.end();
 }
-std::vector<Block>::const_iterator Blockchain::cbegin() const
+template <class T>
+std::vector<Block<T>>::const_iterator Blockchain<T>::cbegin() const
 {
     return v_chain.begin();
 }
-std::vector<Block>::const_iterator Blockchain::cend() const
+template <class T>
+std::vector<Block<T>>::const_iterator Blockchain<T>::cend() const
 {
     return v_chain.end();
 }
-bool Blockchain::valid() const
+template <class T>
+bool Blockchain<T>::valid() const
 {
+    /*
     std::string difficulty(n_difficulty, '0');
     std::string prevHash = "0";
     uint32_t n = 0;
     for (auto &block : v_chain)
     {
-        /* generate hash out of the block and compare*/
         auto curr = block.get_hash();
         auto prev = block.get_prev_hash();
         if (curr.substr(0, n_difficulty) != difficulty ||
             block.get_index() != n || prevHash != prev)
             return false;
         prevHash = curr;
-    }
+    }*/
     return true;
 }
-Block Blockchain::get_last_block() const
+template <class T>
+Block<T> Blockchain<T>::get_last_block() const
 {
     return v_chain.back();
 }
-std::ostream &operator<<(std::ostream &out, const Blockchain &bc)
+
+template <class U>
+std::ostream &operator<<(std::ostream &out, const Blockchain<U> &bc)
 {
     for (size_t i = 0; i < bc.size(); ++i)
         out << bc[i] << (i != bc.size() - 1 ? "\n" : "");
--- Blockchain.h
+++ Blockchain.h
@@ -5,27 +5,30 @@
 #include <algorithm>
 #include "Block.h"
 #include "json.hpp"
-
+#include "uint256.h"
+template <class T>
 class Blockchain
 {
 public:
-    Blockchain(const uint32_t difficulty = 6);
+    Blockchain(const uint64_t difficulty = 6);
     ~Blockchain();
-    void add_block(Block b_new);
-    void set_n_difficulty(const uint32_t difficulty);
-    uint32_t get_n_difficulty() const;
+    void add_block(Block<T> b_new);
+    void set_n_difficulty(const uint64_t difficulty);
+    uint64_t get_n_difficulty() const;
     size_t size() const;
-    Block &operator[](const uint32_t idx);
-    const Block &operator[](const uint32_t idx) const;
-    std::vector<Block>::iterator begin();
-    std::vector<Block>::iterator end();
-    std::vector<Block>::const_iterator cbegin() const;
-    std::vector<Block>::const_iterator cend() const;
+    Block<T>& operator[](const uint64_t idx);
+    const Block<T>& operator[](const uint64_t idx) const;
+    std::vector<Block<T>>::iterator begin();
+    std::vector<Block<T>>::iterator end();
+    std::vector<Block<T>>::const_iterator cbegin() const;
+    std::vector<Block<T>>::const_iterator cend() const;
     bool valid() const;
-    friend std::ostream &operator<<(std::ostream &out, const Blockchain &bc);
+    template <class U>
+    friend std::ostream& operator<<(std::ostream& out, const Blockchain<U>& bc);
 
 private:
-    std::vector<Block> v_chain;
-    uint32_t n_difficulty;
-    Block get_last_block() const;
+    std::vector<Block<T>> v_chain;
+    uint64_t n_difficulty;
+    Block<T> get_last_block() const;
 };
+#include "Blockchain.cpp"
